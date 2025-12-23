f4d225d49b76c761102e529308f9d7b5a76e9f6a
Dhruv Shah <dhruvkshah75@gmail.com>
2025-12-23 21:00:46 +0530

    Dashboard-API which handles user authentication

--- services/dashboard-api/src/index.ts
+++ services/dashboard-api/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from 'elysia';
 import { PrismaClient } from '@prisma/client';
+import { jwt } from '@elysiajs/jwt';
 
 const db = new PrismaClient();
 
 const app = new Elysia() 
+    .get('/', () => 'RateGuard Dashboard API is alive');
+
+    // USER Registration 
